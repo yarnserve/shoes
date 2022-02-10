c482777e5ec3793d7fd11e24ec7b3abd174c3594
yarnserve <yarnserve@gmail.com>
2022-02-10 11:19:32 +0900

    responsiv footer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-// uil-multiply
-
 const bars = document.querySelector(".bars");
 const nav = document.querySelector(".nav");
+const closeBtn = bars.querySelector("i");
+
+const menuItems = document.querySelectorAll(".nav li");
+
+console.log(menuItems);
 
 bars.addEventListener("click", () => {
   nav.classList.toggle("on");
   bars.classList.toggle("on");
+  closeBtn.classList.toggle("uil-multiply");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    nav.classList.toggle("on");
+    bars.classList.toggle("on");
+    closeBtn.classList.toggle("uil-multiply");
+  });
 });
